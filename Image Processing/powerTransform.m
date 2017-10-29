ad4c41b6fd12db64eb9b4e1b3bcdba7d71621577
Rajob Raihan Monmoy <IAmMonmoy@gmail.com>
2017-10-29 22:19:13 +0600

    completed power transformation

---
+++ Image Processing/powerTransform.m
@@ -0,0 +1,21 @@
+img=imread('monmoy.jpg');
+img=rgb2gray(img);
+
+img2=im2double(img);
+[r,c] = size(img2);
+
+factor = 3;
+power= 2;
+
+for i=1:r;
+    for j=1:c
+        img2(i,j) = factor * img2(i,j)^power;
+    end
+end
+
+subplot(1,2,1);
+imshow(img);
+title('Original Image.');
+subplot(1,2,2);
+imshow(img2);
+title('Transformed Image.');
