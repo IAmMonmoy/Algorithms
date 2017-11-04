f2a221463ca499c2e712d538e99e4337c9d9e553
Rajob Raihan Monmoy <IAmMonmoy@gmail.com>
2017-11-04 20:39:17 +0600

    updated contrast stretching

--- Image Processing/contrastStretching.m
+++ Image Processing/contrastStretching.m
@@ -1,4 +1,4 @@
-img = imread('stretch.jpg');
+img = imread('stretch.tif');
 
 maxi = max(img(:));
 mini = min(img(:));
@@ -7,13 +7,17 @@ workImage = img;
 
 workImage = ((workImage-mini))*(255/(maxi-mini));
 
-figure,imshow(img);
-figure,imshow(workImage);
+subplot(2,2,1);
+imshow(img);
+title('original image');
+subplot(2,2,2);
+imshow(workImage);
+title('stretched image');
 
 x = hist_cal(img);
 y = hist_cal(workImage);
 
-subplot(2,1,1);
+subplot(2,2,3);
 stem(x,'marker','none');
-subplot(2,1,2);
+subplot(2,2,4);
 stem(y,'marker','none');
