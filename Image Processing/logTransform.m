20c09ebb40ed3502d09184926613027d5e90e8a2
Rajob Raihan Monmoy <IAmMonmoy@gmail.com>
2017-11-04 20:31:06 +0600

    updated log transformation with iamge

--- Image Processing/logTransform.m
+++ Image Processing/logTransform.m
@@ -1,20 +1,21 @@
-img=imread('monmoy.jpg');
-img=rgb2gray(img);
+img=imread('logTransform.tif');
 
-img2=im2double(img);
-[r,c] = size(img2);
+img2=im2double(img); %because gray level transformation
+[r,c,d] = size(img2);
 
 factor= 2;
+workImage = img2;
 
-for i=1:size(img2,1);
-    for j=1:size(img2,2)
-        img2(i,j) = factor * log(1+img2(i,j));
+for i=1:r
+    for j=1:c
+        workImage(i,j) = factor*log(1+img2(i,j));
     end
 end
+
 
 subplot(1,2,1);
 imshow(img);
 title('Original Image.');
 subplot(1,2,2);
-imshow(img2);
+imshow((workImage));
 title('Transformed Image.');
