3443eb4dd18866f5423b778b011d08272d471e5d
Rajob Raihan Monmoy <IAmMonmoy@gmail.com>
2017-11-04 20:15:03 +0600

    added smoothing filter

--- Image Processing/smoothingLinearFilterAdvanced.m
+++ Image Processing/smoothingLinearFilterAdvanced.m
@@ -1,4 +1,4 @@
-img = imread('smooth.gif');
+img = imread('smooth.tif');
 
 workImage = double(img);
 
@@ -13,11 +13,11 @@ y = floor(y/2);
 finalImage = zeros(r,c,d);
 
 for i = 1:d
-    for j = 2:r-1
-        for k = 2:c-1
+    for j = x+1:r-x
+        for k = y+1:c-y
             for l = -x:x
                 for m = -y:y
-                    finalImage(j,k,i) = finalImage(j,k,i)+(workImage(j+l,k+m,i)*box(l+2,m+2));
+                    finalImage(j,k,i) = finalImage(j,k,i)+(workImage(j+l,k+m,i)*box(l+x+1,m+y+1));
                 end
             end
         end
@@ -33,17 +33,46 @@ x = floor(x/2);
 y = floor(y/2);
 
 for i = 1:d
-    for j = 2:r-1
-        for k = 2:c-1
+    for j = x+1:r-x
+        for k = y+1:c-y
             for l = -x:x
                 for m = -y:y
-                      finalImage1(j,k,i) = finalImage1(j,k,i)+workImage(j+l,k+m,i)*weighted(l+2,m+2);
+                      finalImage1(j,k,i) = finalImage1(j,k,i)+workImage(j+l,k+m,i)*weighted(l+x+1,m+y+1);
                 end
             end
         end
     end
 end
 
-figure,imshow(img);
-figure,imshow(uint8(finalImage)); %box .. more blured
-figure,imshow(uint8(finalImage1)); %weighted less blured
+finalImage2 = zeros(r,c,d);
+
+box=ones(7,7)*(1/49); %3*3 weighted average mask
+
+[x,y] = size(box);
+x = floor(x/2);
+y = floor(y/2);
+
+for i = 1:d
+    for j = x+1:r-x
+        for k = y+1:c-y
+            for l = -x:x
+                for m = -y:y
+                      finalImage2(j,k,i) = finalImage2(j,k,i)+workImage(j+l,k+m,i)*box(l+x+1,m+y+1);
+                end
+            end
+        end
+    end
+end
+
+subplot(2,2,1);
+imshow(img);
+title('real image')
+subplot(2,2,2);
+imshow(uint8(finalImage));
+title('3*3 box mask');
+subplot(2,2,3);
+imshow(uint8(finalImage1));
+title('3*3 weighted average mask');
+subplot(2,2,4);
+imshow(uint8(finalImage2));
+title('7*7 mask');
