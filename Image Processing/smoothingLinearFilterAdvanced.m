ccf12fa5d2a47de9f36d52aa9c42f091601d41d6
Rajob Raihan Monmoy <IAmMonmoy@gmail.com>
2017-11-05 20:00:18 +0600

    updated with zero padding

--- Image Processing/smoothingLinearFilterAdvanced.m
+++ Image Processing/smoothingLinearFilterAdvanced.m
@@ -12,17 +12,18 @@ y = floor(y/2);
 
 finalImage = zeros(r,c,d);
 
-for i = 1:d
-    for j = x+1:r-x
-        for k = y+1:c-y
-            for l = -x:x
-                for m = -y:y
-                    finalImage(j,k,i) = finalImage(j,k,i)+(workImage(j+l,k+m,i)*box(l+x+1,m+y+1));
+for j = 1:r
+    for k = 1:c
+        for l = -x:x
+            for m = -y:y
+                if(j+l >=1 && j+l <= r && k+m >=1 && k+m <= c) %if inside the bound then calculate. Otherwise add nothing
+                    finalImage(j,k) = finalImage(j,k)+(workImage(j+l,k+m)*box(l+x+1,m+y+1));
                 end
             end
         end
     end
 end
+
 
 finalImage1 = zeros(r,c,d);
 
@@ -32,37 +33,40 @@ weighted=[1 2 1; 2 4 2; 1 2 1]*(1/16); %3*3 weighted average mask
 x = floor(x/2);
 y = floor(y/2);
 
-for i = 1:d
-    for j = x+1:r-x
-        for k = y+1:c-y
-            for l = -x:x
-                for m = -y:y
-                      finalImage1(j,k,i) = finalImage1(j,k,i)+workImage(j+l,k+m,i)*weighted(l+x+1,m+y+1);
+
+for j = x+1:r-x
+    for k = y+1:c-y
+        for l = -x:x
+            for m = -y:y
+                if(j+l >=1 && j+l <= r && k+m >=1 && k+m <= c) %if inside the bound then calculate. Otherwise add nothing
+                  finalImage1(j,k) = finalImage1(j,k)+workImage(j+l,k+m)*weighted(l+x+1,m+y+1);
                 end
             end
         end
     end
 end
 
+
 finalImage2 = zeros(r,c,d);
 
-box=ones(7,7)*(1/49); %3*3 weighted average mask
+box=ones(7,7)*(1/49); 
 
 [x,y] = size(box);
 x = floor(x/2);
 y = floor(y/2);
 
-for i = 1:d
-    for j = x+1:r-x
-        for k = y+1:c-y
-            for l = -x:x
-                for m = -y:y
-                      finalImage2(j,k,i) = finalImage2(j,k,i)+workImage(j+l,k+m,i)*box(l+x+1,m+y+1);
+for j = x+1:r-x
+    for k = y+1:c-y
+        for l = -x:x
+            for m = -y:y
+                if(j+l >=1 && j+l <= r && k+m >=1 && k+m <= c) %if inside the bound then calculate. Otherwise add nothing
+                  finalImage2(j,k) = finalImage2(j,k)+workImage(j+l,k+m)*box(l+x+1,m+y+1);
                 end
             end
         end
     end
 end
+
 
 subplot(2,2,1);
 imshow(img);
